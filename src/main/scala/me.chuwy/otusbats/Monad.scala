7a0e6cb6be97da49b6620bd8d122b5ab88dd468f
borisenkoaa <a.borisenko@svetets.ru>
2022-02-21 17:42:33 +0300

    homework

--- src/main/scala/me.chuwy/otusbats/Monad.scala
+++ src/main/scala/me.chuwy/otusbats/Monad.scala
@@ -5,9 +5,28 @@ trait Monad[F[_]] extends Functor[F] { self =>
 
   def point[A](a: A): F[A]
 
-  def flatten[A](fa: F[F[A]]): F[A]
+  def flatten[A](fa: F[F[A]]): F[A] = self.flatMap(fa)(a => a)
 }
 
 object Monad {
+  implicit def optionMonad: Monad[Option] = new Monad[Option] {
+    override def flatMap[A, B](fa: Option[A])(f: A => Option[B]): Option[B] = fa.flatMap(f)
+    override def point[A](a: A): Option[A] = Option(a)
+    override def map[A, B](fa: Option[A])(f: A => B): Option[B] = fa.map(f)
+  }
 
+  implicit def listMonad: Monad[List] = new Monad[List] {
+    override def flatMap[A, B](fa: List[A])(f: A => List[B]): List[B] = fa.flatMap(f)
+    override def point[A](a: A): List[A] = List(a)
+    override def map[A, B](fa: List[A])(f: A => B): List[B] = fa.map(f)
+  }
+
+  type EitherT[A] = Either[Any, A]
+  implicit def eitherMonad: Monad[EitherT] = new Monad[EitherT] {
+    override def flatMap[A, B](fa: EitherT[A])(f: A => EitherT[B]): EitherT[B] = fa.flatMap(f)
+
+    override def point[A](a: A): EitherT[A] = Right(a)
+
+    override def map[A, B](fa: EitherT[A])(f: A => B): EitherT[B] = fa.map(f)
+  }
 }
--- src/main/scala/me.chuwy/otusbats/Show.scala
+++ src/main/scala/me.chuwy/otusbats/Show.scala
@@ -1,6 +1,5 @@
 package me.chuwy.otusbats
 
-
 trait Show[A] {
   def show(a: A): String
 }
@@ -9,20 +8,26 @@ object Show {
 
   // 1.1 Instances (`Int`, `String`, `Boolean`)
 
+  implicit val intShow: Show[Int] = fromJvm[Int]
+  implicit val stringShow: Show[String] = (a: String) => a
+  implicit val booleanShow: Show[Boolean] = fromJvm[Boolean]
 
   // 1.2 Instances with conditional implicit
 
-  implicit def listShow[A](implicit ev: Show[A]): Show[List[A]] =
-    ???
+  implicit def listShow[A](implicit ev: Show[A]): Show[List[A]] = new Show[List[A]] {
+    override def show(a: List[A]): String = a.foldLeft("")( (acc,a) => acc ++ ev.show(a))
+  }
 
 
   // 2. Summoner (apply)
+
+  def apply[A](implicit ev:Show[A]): Show[A] = ev
 
   // 3. Syntax extensions
 
   implicit class ShowOps[A](a: A) {
     def show(implicit ev: Show[A]): String =
-      ???
+      ev.show(a)
 
     def mkString_[B](begin: String, end: String, separator: String)(implicit S: Show[B], ev: A <:< List[B]): String = {
       // with `<:<` evidence `isInstanceOf` is safe!
@@ -39,16 +44,20 @@ object Show {
    *  @param begin. '[' in above example
    *  @param end. ']' in above example
    */
-  def mkString_[A: Show](list: List[A], begin: String, end: String, separator: String): String =
-    ???
-
+  def mkString_[A: Show](list: List[A], begin: String, end: String, separator: String): String = list.headOption.map { head =>
+    list.tail.foldLeft(begin ++ head.show)((acc, a) => acc ++ separator ++ a.show) ++ end
+  }.getOrElse("")
 
   // 4. Helper constructors
 
   /** Just use JVM `toString` implementation, available on every object */
-  def fromJvm[A]: Show[A] = ???
+  def fromJvm[A]: Show[A] = new Show[A] {
+    override def show(a: A): String = a.toString
+  }
   
   /** Provide a custom function to avoid `new Show { ... }` machinery */
-  def fromFunction[A](f: A => String): Show[A] = ???
+  def fromFunction[A](f: A => String): Show[A] = new Show[A]{
+    override def show(a: A): String = f(a)
+  }
 
 }
